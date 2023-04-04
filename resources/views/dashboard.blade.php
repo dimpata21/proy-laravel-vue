cd4871cf9f27a4c73c033f0956becbf3831fee78
Diego Armando Salazar Impatá <dimpata@hotmail.com>
2023-04-03 20:42:32 -0500

    terminado con frontend

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,46 @@
+{{-- Llamamos la vista de la cual heredamos la estructura --}}
+@extends('dashboard.master')
+@section('titulo', 'Agregar Post')
+@section('contenido')
+@include('dashboard.partials.validation-error')
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div>
+        <div>
+            <section class="p-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md">
+                            <div class="card bg-dark text-light">
+                                <div class="card-body text-center">
+                                    <div class="h1 mb-3">
+                                        <i class="bi bi-laptop"></i>
+                                    </div>
+                                    <a href="{{ url('/dashboard/post') }}" class="btn btn-primary">Publicaciones</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="card bg-secondary text-light">
+                                <div class="card-body text-center">
+                                    <div class="h1 mb-3">
+                                        <i class="bi bi-person-square"></i>
+                                    </div>
+                                    <a href="{{ url('/dashboard/category') }}" class="btn btn-dark">Categorias</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+
+
+    </div>
